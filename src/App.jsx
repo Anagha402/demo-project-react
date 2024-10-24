d5952aaa412df1eb0ba4c41711025972d28049fc
Anagha402 <anagharamesh18@gmail.com>
2024-10-24 11:29:15 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './Header'
+import Footer from './Footer'
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <h1>App Component</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quae assumenda laudantium facilis repellendus delectus ullam, adipisci error, doloremque magnam quibusdam sequi. Sunt corporis perspiciatis id eaque vitae necessitatibus quis!
+      Voluptatum esse aut incidunt? Esse exercitationem voluptates sit. Itaque magnam dolor beatae nam. Vitae minima, maiores assumenda nisi explicabo, alias nostrum, est ipsa autem cum iste. Ad nulla eum facilis.
+      </p>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
